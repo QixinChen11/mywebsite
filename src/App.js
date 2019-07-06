703da81b7252546143c4ea59a3ec4f6e7f624fca
QixinChen11 <qixin.ch3n@gmail.com>
2019-07-05 20:08:31 -0400

    Template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Icon } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="bar">
+          <div className="title">Qixin Chen</div>
+          
+          <div className="links">
+            <a className="a" href="https://github.com/QixinChen11" rel="noopener noreferrer" target="_blank">
+              <Icon circular inverted name='github' />
+            </a>
+            <a className="a" href="https://www.linkedin.com/in/qixin-chen-01a191156/" rel="noopener noreferrer" target="_blank">
+              <Icon circular inverted name='linkedin' />
+            </a>
+            <a className="a" href="mailto:qixin.ch3n@gmail.com" rel="noopener noreferrer" target="_blank">
+              <Icon circular inverted name='mail outline' />
+            </a>
+          </div>
+          
+          <div className="About_me">
+            <a className="a" href='#About_me'>
+                About Me
+            </a>
+          </div>
+          <div>
+            <a className="a" href='#Experiences'>
+              <div className="Experiences">
+                Experiences
+              </div>
+            </a>
+          </div>
+          <div>
+            <a className="a" href='#Skills'>
+              <div className="Skills">
+                Skills
+              </div>
+            </a>
+          </div>
+          <div>
+            <a className="a" href='#Projects'>
+              <div className="Projects">
+                Projects
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="content">
+          <div id='About_me'>
+          
+          </div>
+          <div id='Experiences'>
+          
+          </div>
+          <div id='Skills'>
+          
+          </div>
+          <div id='Projects'>
+          
+          </div>
+        </div>
       </div>
     );
   }
