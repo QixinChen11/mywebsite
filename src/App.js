d7f21430d5f3acde85b038ee300e249246b3f875
OmegaPiece123 <qchen5249@bths.edu>
2019-07-19 15:14:20 -0400

    added mobile styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { Icon } from 'semantic-ui-react'
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      highlight: ""
+    }
+  }
+
   makeStars = (num) => {
     let star = "\u2605";
     let unfinished_star = "\u2606";
@@ -15,51 +22,80 @@ class App extends Component {
     );
   }
 
+  changeClick = (e) => {
+    this.setState({highlight: e})
+  }
+  
+
   render() {
+    let pButton = "not-clicked-button";
+    let edButton = "not-clicked-button";
+    let exButton = "not-clicked-button";
+    let sButton = "not-clicked-button";
+    let proButton = "not-clicked-button";
+    switch (this.state.highlight) {
+      case "Profile":
+        pButton = "clicked-button";
+        break;
+      case "Education":
+        edButton = "clicked-button";
+        break;
+      case "Projects":
+        proButton = "clicked-button";
+        break;
+      case "Skills":
+        sButton = "clicked-button";
+        break;
+      case "Experiences":
+        exButton = "clicked-button";
+        break;
+      default:
+        break;
+    }
     return (
       <div className="app">
         <div className="bar">
           <div className="title">Qixin Chen</div>
           <div className="profile_pic"><img className="me"alt="me" src={require('./me.jpg')} /></div>
           <div className="links">
-          <a className="a" href="https://github.com/QixinChen11" rel="noopener noreferrer" target="_blank">
-            <Icon size="large" circular inverted name='github' />
-          </a>
-          <a className="a" href="https://www.linkedin.com/in/qixin-chen-01a191156/" rel="noopener noreferrer" target="_blank">
-            <Icon size="large" circular inverted name='linkedin' />
-          </a>
+            <a className="a" href="https://github.com/QixinChen11" rel="noopener noreferrer" target="_blank">
+              <Icon size="large" circular inverted name='github' />
+            </a>
+            <a className="a" href="https://www.linkedin.com/in/qixin-chen-01a191156/" rel="noopener noreferrer" target="_blank">
+              <Icon size="large" circular inverted name='linkedin' />
+            </a>
           </div>
           <div>
-            <a className="a" href='#Profile'>
-              <div className="Profile">
+            <a className="a" href='#Profile' onClick={()=> this.changeClick("Profile")}>
+              <div id={pButton} className="Profile">
                 Profile
               </div>
             </a>
           </div>
           <div>
-            <a className="a" href='#Education'>
-              <div className="Education">
+            <a className="a" href='#Education' onClick={()=> this.changeClick("Education")}>
+              <div id={edButton} className="Education">
                 Education
               </div>
             </a>
           </div>
           <div>
-            <a className="a" href='#Experiences'>
-              <div className="Experiences">
+            <a  className="a" href='#Experiences' onClick={()=> this.changeClick("Experiences")}>
+              <div id={exButton} className="Experiences">
                 Experiences
               </div>
             </a>
           </div>
           <div>
-            <a className="a" href='#Skills'>
-              <div className="Skills">
+            <a className="a" href='#Skills' onClick={()=> this.changeClick("Skills")}>
+              <div id={sButton} className="Skills">
                 Skills
               </div>
             </a>
           </div>
           <div>
-            <a className="a" href='#Projects'>
-              <div className="Projects">
+            <a  className="a" href='#Projects' onClick={()=> this.changeClick("Projects")}>
+              <div id={proButton} className="Projects">
                 Projects
               </div>
             </a>
@@ -71,6 +107,15 @@ class App extends Component {
             <div className="content-about">
               <div className="about">
                 <div className="about-title">About Me</div>
+                <div className="profile_pic"><img className="me"alt="me" src={require('./me.jpg')} /></div>
+                <div className="links">
+                  <a className="a" href="https://github.com/QixinChen11" rel="noopener noreferrer" target="_blank">
+                    <Icon size="large" circular inverted name='github' />
+                  </a>
+                  <a className="a" href="https://www.linkedin.com/in/qixin-chen-01a191156/" rel="noopener noreferrer" target="_blank">
+                    <Icon size="large" circular inverted name='linkedin' />
+                  </a>
+                </div>
                 <div className="about-content">
                   Hi, my name is Qixin Chen. I am a senior at Stony Brook University studying Computer Science.
                   My main set of skills are fullstack web development, JS, Java, and Python. I would love to be involved in game development.
